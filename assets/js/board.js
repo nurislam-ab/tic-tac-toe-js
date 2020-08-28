bfd06b614e4e96e0f1b7d3017ecb940f3b166661
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-08-28 17:56:17 -0500

    Added delete method for restarting the board

--- assets/js/board.js
+++ assets/js/board.js
@@ -1,23 +1,32 @@
+import GameLogic from './game.js';
+
 const GameBoard = (() => {
-
+  
   const displayBoard = () => {
+    
     let mainElement = document.getElementById('main');
-
     let container = document.createElement('div');
     container.id = "gameboard";
     mainElement.append(container);
 
-    for(let i = 1; i <= 9; i++){
+    for(let i = 0; i <= 8; i++){
       let cellWrapper = document.createElement('div');
+      cellWrapper.onclick = function(){GameLogic.playerInput(i)};
       cellWrapper.id = `${i}`;
-      cellWrapper.innerHTML = `${i}`;
       container.appendChild(cellWrapper);
     }
   }
 
+  const deleteBoard = (() => {
+    let board = document.getElementById('gameboard');
+    if (board != null) {
+      board.parentNode.removeChild(board);
+    } 
+  })
 
   return {
-    displayBoard
+    displayBoard,
+    deleteBoard
   };
 })();
 
